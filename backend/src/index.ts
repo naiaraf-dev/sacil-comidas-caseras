fc764c62975f28ea82ff1a50119f0375e45151b8
naiaraf-dev <naiaraf.dev@gmail.com>
2026-02-04 17:13:02 -0300

    fix: use dynamic port and prepare CORS for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,16 +3,27 @@ import productosRouter from "./routes/productos.routes";
 import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+
+app.use(
+    cors({
+        origin: [
+        "http://localhost:5173",
+        // después agregamos acá el dominio de Vercel
+        ],
+    })
+);
+
+// servir imágenes
 app.use("/img", express.static("public/img"));
 
 // rutas
 app.use("/productos", productosRouter);
 
-// iniciar servidor
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
+// puerto dinámico (OBLIGATORIO en prod)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
